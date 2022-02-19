2d45afc2820a4666e8f5ffcacc6fa3c558d66dfb
azeezh <ah88238@uga.edu>
2022-02-19 04:18:43 -0500

    version with change text for 2
    Changes text twice

--- SceneTemplate1.js
+++ SceneTemplate1.js
@@ -1,19 +1,29 @@
 var i = 0;
 var speed = 75;
-var txt = "testing ";
 var loop = null;
+var textBox = document.getElementById("animatedText");
+var txt = "You crack your eyes open to a gentle sunrise filtering through your windows, illuminating your extraordinarily messy room.";
+var txt2 = "You quickly run down, grab a piece of toast and shove it in your mouth, and run out the door.";
 
+var first = 0;
 
 function startAnimation() {
-    loop = setInterval(
-        function textAnimation() {
-            if (i < txt.length) {
-                document.getElementById("animatedText").innerHTML += txt.charAt(i);
-                i++;
-            } else {
-                stopAnimation();
-            }
-        }, speed);
+    if (first == 0) {
+        first = 1;
+        textBox.innerHTML = "";
+        loop = setInterval(
+            function textAnimation() {
+                if (i < txt.length) {
+                    textBox.innerHTML += txt.charAt(i);
+                    i++;
+                } else {
+                    stopAnimation();
+                }
+            }, speed);
+        }
+        else if (first == 1) {
+            stopAnimation();
+        }
 }
 
 
@@ -21,7 +31,7 @@ function startAnimation() {
 function stopAnimation() {
     clearInterval(loop);
     i = 0;
-    document.getElementById("animatedText").innerHTML += "ENDED";
-    document.getElementById("Textbox").removeChild;
-    document.getElementById("Textbox").appendChild(startAnimation);
+    first = 0;
+    textBox.innerHTML = txt;
+    txt = txt2;
 }
