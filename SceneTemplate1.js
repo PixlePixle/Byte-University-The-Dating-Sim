2c5f2d8e89fadb811b1e74e095cce8ae803d31fb
pixIe <67672793+PixlePixle@users.noreply.github.com>
2022-02-19 06:10:47 -0500

    Adding choices
    Updating javascript to add choices!

--- SceneTemplate1.js
+++ SceneTemplate1.js
@@ -1,7 +1,9 @@
 var i = 0;
-var speed = 75;
+var speed = 25;
 var loop = null;
 var textBox = document.getElementById("animatedText");
+var button = document.getElementById("Textbox");
+var options = document.getElementById("Options");
 const txtArray = ["You crack your eyes open to a gentle sunrise filtering through your windows, illuminating your extraordinarily messy room.",
 "<i>Argh, what time is it?</i> you wonder to yourself. You roll over and click your alarm off. The time reads 7:57 am.",
 "…", 
@@ -9,10 +11,17 @@ const txtArray = ["You crack your eyes open to a gentle sunrise filtering throug
 "You quickly run down, grab a piece of toast and shove it in your mouth, and run out the door."]
 txt = txtArray[0];
 
+var choiceNumber = 0;
+const choices = ["1", "2", "3", "4",
+                 "5", "6", "7", "8",
+                 "9","10","11","12"];
+
 var first = 0;
 var italicized = false;
+var makeChoice = false;
 
 function startAnimation() {
+    
     if (first == 0) {
         first = 1;
         textBox.innerHTML = "";
@@ -35,10 +44,10 @@ function startAnimation() {
                     stopAnimation();
                 }
             }, speed);
-        }
-        else if (first == 1) {
-            stopAnimation();
-        }
+    } else if (first == 1) {
+        stopAnimation();
+    } else;
+
 }
 
 var index = 1;
@@ -46,10 +55,30 @@ var index = 1;
 function stopAnimation() {
     clearInterval(loop);
     i = 0;
-    first = 0;
+
     textBox.innerHTML = txt;
-    txt = txtArray[index];
-    index++;
+    italicized = false;
+    first = 0;
+    if (index < txtArray.length) {
+        txt = txtArray[index];
+        index++;
+        
+    } else {
+        options.style.display = "block";
+        index = 0;
+        first = 20;
+        makeChoice();
+
+    }
+}
+
+
+
+function makeChoice() {
+    document.getElementById("Option1").innerHTML = choices[choiceNumber++];
+    document.getElementById("Option2").innerText = choices[choiceNumber++];
+    document.getElementById("Option3").innerText = choices[choiceNumber++];
+    document.getElementById("Option4").innerText = choices[choiceNumber++];
 }
 
 function changeImage(a) {
